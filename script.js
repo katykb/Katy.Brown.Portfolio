1ff90a6abaa381fda4b199825b856c706546a818
Katy Brown <katybr35@gmail.com>
2022-07-02 20:39:08 -0500

    taking out form code

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-window.onscroll = function() {myFunction()};
+// window.onscroll = function () {
+//   myFunction();
+// };
 
-const navbar = document.getElementById("navbar");
-const sticky = navbar.offsetTop;
+// const navbar = document.getElementById("navbar");
+// const sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky");
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+
+// document
+//   .getElementById("contactBtn")
+//   .addEventListener("click", function sendEmail() {
+//     console.log("button hit");
+//     Email.send({
+//       Host: "smtp.gmail.com",
+//       Username: "katybr35@gmail.com",
+//       Password: "Ethanhenry01!",
+//       To: "katybr@gmail.com",
+//       From: document.getElementById("email").value,
+//       Subject: "New contact form inquiry",
+//       Body:
+//         "Name: " +
+//         document.getElementById("name").value +
+//         " " +
+//         "Email: " +
+//         document.getElementById("email").value +
+//         " " +
+//         "Message: " +
+//         document.getElementById("message").value,
+       
+//     }).then((message) => alert(message));
+   
+//     return false;
+//   });
